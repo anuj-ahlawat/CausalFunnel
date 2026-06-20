ac3ddeff2c7bbaca6bae5aa7ea984470c08d5e54
anuj ahlawat <anujahlawat1024@gmail.com>
2026-06-20 11:31:53 +0530

    Fix tracker.js static path

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,24 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Serve tracker.js from backend/public
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok' });
-});
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-
-const eventRoutes = require('./routes/eventRoutes');
-const { notFound, errorHandler } = require('./middleware/errorMiddleware');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
 // Explicitly serve tracker.js
 app.get('/tracker.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/tracker.js'));
@@ -40,12 +22,6 @@ app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-app.use('/api', eventRoutes);
-
-app.use(notFound);
-app.use(errorHandler);
-
-module.exports = app;
 app.use('/api', eventRoutes);
 
 app.use(notFound);
